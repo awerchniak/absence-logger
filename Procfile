fc79eaed073c34f06ef6b65c9e730953ceeb9cba
awerchniak <andy.werchniak@gmail.com>
2019-03-15 20:16:36 -0400

    Configure for heroku
    Changes to be committed:
    	new file:   Procfile
    	modified:   absence_logger/settings/base.py
    	renamed:    hall_pass/migrations/0001_squashed_0005_auto_20190311_2045.py -> hall_pass/migrations/0001_initial.py
    	new file:   requirements.txt
    	modified:   requirements/base.txt

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn absence_logger.wsgi
--- absence_logger/settings/base.py
+++ absence_logger/settings/base.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/2.1/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/2.1/ref/settings/
 """
-
+import django_heroku
 import os
 
 from django.core.exceptions import ImproperlyConfigured
@@ -127,3 +127,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
